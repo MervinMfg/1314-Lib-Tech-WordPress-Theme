958b2122664d30dd66365070e1bfc55b54440498
Brian Behrens <brian@brainbrian.com>
2013-11-19 16:39:37 -0800

    updated team detail pages. now pull from team-detail.php include and have updated layouts

--- _/js/libtech.main.js
+++ _/js/libtech.main.js
@@ -1001,6 +1001,37 @@ LIBTECH.main = {
         var self = this;
         // render social content grid items
         self.utilities.socialContentGridItemsInit();
+        // init gallery
+        if ($('.gallery')) {
+            self.utilities.galleryInit();
+        }
+        // assign a click event to the video thumbnails
+        $('.video-thumbnails li a').click(function () {
+            var videoID, videoType, videoPlayerHTML;
+            videoID = $(this).attr('data-video-id');
+            videoType = $(this).attr('data-video-type');
+            // select the right thumbnail
+            $('.video-thumbnails li a').removeClass('selected');
+            $(this).addClass('selected');
+            // display the video info
+            $('.video-info').removeClass('selected');
+            $('.video-player #' + videoID).addClass('selected');
+            // add the video content
+            if (videoType === "YouTube") {
+                videoPlayerHTML = '<iframe width="620" height="348" src="http://www.youtube.com/embed/' + videoID + '" frameborder="0" allowfullscreen></iframe>';
+            } else if (videoType === "Vimeo") {
+                videoPlayerHTML = '<iframe src="http://player.vimeo.com/video/' + videoID + '" width="620" height="348" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+            }
+            $('.video-player .frame-wrapper').html(videoPlayerHTML);
+            // make video fit within target
+            $('.video-player .frame-wrapper').fitVids();
+            // kill the links default behavior
+            return false;
+        });
+        // select the first video
+        $('.video-thumbnails li a:first').click();
+        // make video fit within target
+        $('.video-player .frame-wrapper').fitVids();
     },
     shoppingCartInit: function () {
         var self, lang, regionCookie;
