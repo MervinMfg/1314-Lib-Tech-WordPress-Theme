1049820ce74be6d9f57e6e15c48a0eb3ae35da98
Brian Behrens <brian@brainbrian.com>
2014-04-13 23:31:41 -0700

    fixed image path for social grid

--- _/js/libtech.main.js
+++ _/js/libtech.main.js
@@ -1405,7 +1405,7 @@ LIBTECH.main = {
 							postImage = postData.picture;
 							postImage = postImage.replace("_s", "_n");
 							// set up facebook list item
-							listItem = '<li class="grid-item facebook item-' + totalItems + '"><div class="grid-item-wrapper"><a href="' + postData.link + '" target="_blank"><div class="facebook-wrapper"><div class="facebook-header"><div class="facebook-profile"><img src="https://graph.facebook.com/' + facebookUsername + '/picture" /></div><p class="facebook-name">' + postData.from.name + '</p><p class="facebook-time">' + postDate + '</p><div class="clearfix"></div></div><div class="facebook-photo"><img src="' + postImage + '" /></div><p class="facebook-excerpt">' + postData.message + '</p></div><div class="facebook-aspect-ratio"><img src="/wp-content/themes/libtech_2/_/img/square.gif" /></div><div class="clearfix"></div></a></div></li>';
+							listItem = '<li class="grid-item facebook item-' + totalItems + '"><div class="grid-item-wrapper"><a href="' + postData.link + '" target="_blank"><div class="facebook-wrapper"><div class="facebook-header"><div class="facebook-profile"><img src="https://graph.facebook.com/' + facebookUsername + '/picture" /></div><p class="facebook-name">' + postData.from.name + '</p><p class="facebook-time">' + postDate + '</p><div class="clearfix"></div></div><div class="facebook-photo"><img src="' + postImage + '" /></div><p class="facebook-excerpt">' + postData.message + '</p></div><div class="facebook-aspect-ratio"><img src="/wp-content/themes/libtech/_/img/square.gif" /></div><div class="clearfix"></div></a></div></li>';
 							// add list item to content grid
 							$('.content-grid ul').append(listItem);
 							totalItems++;
